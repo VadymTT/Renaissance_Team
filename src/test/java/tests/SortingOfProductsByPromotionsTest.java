66a1990f21c590c12595c549c0ff6ea195a46449
DashaYab <dash.yab@yahoo.com>
2023-07-05 19:30:00 +0300

    Add TR-15

---
+++ src/test/java/tests/SortingOfProductsByPromotionsTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import base.AbstractBaseTest;
+import io.qameta.allure.Description;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class SortingOfProductsByPromotionsTest extends AbstractBaseTest {
+    @Test
+    @Description("TR-15/Test Case 3. Sorting of products by promotions.")
+    public void checkSortingOfProductByPromotions() {
+        HomePage homePage = new HomePage(driver);
+    }
+}
